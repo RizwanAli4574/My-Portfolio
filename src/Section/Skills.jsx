a355c09dadad94ee623aa8123cbf267492d914e6
Rizwan Ali <rizwanali4579897@gmail.com>
2025-11-25 01:30:51 +0500

    Some work

--- src/Section/Skills.jsx
+++ src/Section/Skills.jsx
@@ -10,21 +10,100 @@ import canva from "../assets/Skills/Canva.png";
 import "../styles/Skills.css";
 
 function Skills() {
-  const skills = [
+  const fskills = [
     htmlpic, csspic, jspic, reactPic, tailwind,
-    bootStrap, word, excel, canva
+    bootStrap,
   ];
+
+  const oskills = [
+    word, excel, canva
+  ];
+
+  // const fskills = [
+  //   htmlpic, csspic, jspic, reactPic, tailwind,
+  //   bootStrap, word, excel, canva
+  // ];
 
   return (
     <section className="bg-black md:py-40 pt-20 pb-10">
       <h2 className="text-4xl font-bold text-center text-white mb-30">
+       Frontend Skills
+      </h2>
+
+      <div className="scroll-container">
+        <div className="scroll-items">
+
+         
+          {fskills.map((img, idx) => (
+            <img
+              key={idx}
+              src={img}
+              alt="Skill"
+              className="skill-img"
+            />
+          ))}
+
+         
+          {fskills.map((img, idx) => (
+            <img
+              key={idx + fskills.length}
+              src={img}
+              alt="Skill"
+              className="skill-img"
+            />
+          ))}
+         
+          {fskills.map((img, idx) => (
+            <img
+              key={idx + fskills.length}
+              src={img}
+              alt="Skill"
+              className="skill-img"
+            />
+          ))}
+
+        </div>
+      </div>
+
+
+      <h2 className="text-4xl font-bold text-center text-white mb-30 pt-15">
+       Other Skills
+      </h2>
+
+      <div className="scroll-container">
+        <div className="scroll-items">
+
+          {oskills.map((img, idx) => (
+            <img
+              key={idx}
+              src={img}
+              alt="Skill"
+              className="skill-img"
+            />
+          ))}
+
+      
+          {oskills.map((img, idx) => (
+            <img
+              key={idx + oskills.length}
+              src={img}
+              alt="Skill"
+              className="skill-img"
+            />
+          ))}
+
+        </div>
+      </div>
+
+
+      {/* <h2 className="text-4xl font-bold text-center text-white mb-30">
         Skills
       </h2>
 
       <div className="scroll-container">
         <div className="scroll-items">
 
-          {/* First Set */}
+         
           {skills.map((img, idx) => (
             <img
               key={idx}
@@ -34,7 +113,7 @@ function Skills() {
             />
           ))}
 
-          {/* Duplicate Set for infinite loop */}
+         
           {skills.map((img, idx) => (
             <img
               key={idx + skills.length}
@@ -45,65 +124,7 @@ function Skills() {
           ))}
 
         </div>
-      </div>
-      <h2 className="text-4xl font-bold text-center text-white mb-30">
-        Skills
-      </h2>
-
-      <div className="scroll-container">
-        <div className="scroll-items">
-
-          {/* First Set */}
-          {skills.map((img, idx) => (
-            <img
-              key={idx}
-              src={img}
-              alt="Skill"
-              className="skill-img"
-            />
-          ))}
-
-          {/* Duplicate Set for infinite loop */}
-          {skills.map((img, idx) => (
-            <img
-              key={idx + skills.length}
-              src={img}
-              alt="Skill"
-              className="skill-img"
-            />
-          ))}
-
-        </div>
-      </div>
-      <h2 className="text-4xl font-bold text-center text-white mb-30">
-        Skills
-      </h2>
-
-      <div className="scroll-container">
-        <div className="scroll-items">
-
-          {/* First Set */}
-          {skills.map((img, idx) => (
-            <img
-              key={idx}
-              src={img}
-              alt="Skill"
-              className="skill-img"
-            />
-          ))}
-
-          {/* Duplicate Set for infinite loop */}
-          {skills.map((img, idx) => (
-            <img
-              key={idx + skills.length}
-              src={img}
-              alt="Skill"
-              className="skill-img"
-            />
-          ))}
-
-        </div>
-      </div>
+      </div> */}
     </section>
   );
 }
